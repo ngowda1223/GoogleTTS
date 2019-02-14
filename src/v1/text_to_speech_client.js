aeeb6d81081bcb89c536fe15ea7d22cb9050cd4d
Dave Gramlich <callmehiphop@gmail.com>
2019-02-14 15:37:52 -0500

    Revert "[CHANGE ME] Re-generated  to pick up changes in the API or client library generator. (#189)"
    This reverts commit f9b774b20e647ab87e2d0b652658c71a5fb9c433.

--- src/v1/text_to_speech_client.js
+++ src/v1/text_to_speech_client.js
@@ -126,10 +126,6 @@ class TextToSpeechClient {
             function() {
               const args = Array.prototype.slice.call(arguments, 0);
               return stub[methodName].apply(stub, args);
-            },
-          err =>
-            function() {
-              throw err;
             }
         ),
         defaults[methodName],
